f7344c4542e00a1428115710a28e5344d1d3f17a
Ayesha Siddika <ayeshasiddika777697@gmail.com>
2024-12-24 13:41:16 +0600

    get and post all foods data

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_pass}@cluster0.331jm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("restaurant_DB");
+    const foodsCollections = database.collection("restaurant_DB");
+
+
+    app.get('/foods',async(req,res)=>{
+        const cursor = foodsCollections.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.post('/foods',async (req,res)=>{
+        const newfood = req.body;
+        const result = await foodsCollections.insertOne(newfood)
+        res.send(result)
+
+    })
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send('resturant server is running')
+})
+app.listen(port, ()=>{
+    console.log('server is running');
+    
+})
